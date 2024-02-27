89ad8f7ddb979aa099d511df56bcef24e9b904c2
pepguiem <pepguiemnolla@gmail.com>
2024-02-27 15:50:03 +0100

    asi deberia ir

--- src/js/script.js
+++ src/js/script.js
@@ -79,7 +79,7 @@ function createCard(game) {
       </ul>
       <div class="card-body">
       <button type="button" class="btn btn-warning fw-bold" onclick="addToCart('${game.name}', '${price}')">PURCHASE</button>
-      <a href="./game" onclick="showGame('${game.id}')><button type="button" class="btn btn-primary" ">View</button></a>
+      <button type="button" class="btn btn-primary" onclick="showGame('${game.id}')">View</button>
       </div>
   `;
   return cardDiv;
@@ -120,11 +120,10 @@ function calculatePrice(releaseDate) {
 }
 
 //Función para poner la info de la card del game del index.html en
-function showGame(event, id){
-  event.preventDefault();
+function showGame( id){
   var game = games.find(game => game.id == id);
   localStorage.setItem('game', JSON.stringify(game))
-  window.location.href = event.target.href;
+  window.location.href = "./game";
 }
 
 
